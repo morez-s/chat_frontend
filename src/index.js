a753a01d98930f153cc21c26d7a72c5661f1c052
morez <m.sahelgozin@gmail.com>
2021-12-01 20:42:12 +0330

    home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
+import './assets/scss/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,24 @@
+import { Button } from 'antd';
+import { useNavigate } from "react-router-dom";
+import { UserAddOutlined } from '@ant-design/icons';
+import { LoginOutlined } from '@ant-design/icons';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="home-container">
+      <h1 className="site-title">نرم افزار چت آنلاین</h1>
+
+      <Button type="primary" shape="round" size="large" icon={<UserAddOutlined />} onClick={() => navigate("/registration")}>
+        ثبت نام
+      </Button>
+      
+      <Button type="primary" shape="round" size="large" icon={<LoginOutlined />} onClick={() => navigate("/login")}>
+        ورود
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
