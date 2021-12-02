04e84f221685cc52359c10653296ae0452f569c9
morez <m.sahelgozin@gmail.com>
2021-12-02 10:50:08 +0330

    chat area ui

--- src/components/ChatArea.js
+++ src/components/ChatArea.js
@@ -1,7 +1,24 @@
+import MessageBox from './MessageBox.js';
+import { Button, Input } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+
 const ChatArea = () => {
   return (
     <div className="chat-area">
-      <div>chat area</div>
+      <div className="messages">
+        <MessageBox side="sent" />
+        <MessageBox side="sent" />
+        <MessageBox side="received" />
+        <MessageBox side="sent" />
+      </div>
+
+      <div className="input-container">
+        <Input className="input" size="large" onKeyPress={e => e.key === 'Enter' && alert('send')} />
+
+        <Button className="button" type="primary" shape="round" size="large" icon={<ArrowLeftOutlined />} onClick={() => alert('send')}>
+          ارسال
+        </Button>
+      </div>
     </div>
   );
 };
